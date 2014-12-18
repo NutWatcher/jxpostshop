b718a82179911e934e340152f8d76949248bd570
yzjf <123123>
2014-12-18 16:40:12 +0800

    20141218

--- dao/goodsDao.js
+++ dao/goodsDao.js
@@ -3,8 +3,8 @@
  */
 var baseDb = require('./baseDao');
 exports.getGoods = function (type_id, cb) {
-    cb(null,[{"name":"一个","price":30,"image":"/images/good1.jpg","href":"http://localhost:3000/good/00001.html"},
-        {"name":"2个","price":60,"image":"/images/good1.jpg","href":"http://localhost:3000/good/00002.html"}]);
+    cb(null,[{"id":"12580","name":"一个神奇的购物单","price":30,"image":"/images/good1.jpg","href":"http://localhost:3000/good/00001.html"},
+        {"id":"12590","name":"2水电费水电费速度放电饭锅的风格的风格放电饭锅的风格的风格放电饭锅的风格的风格放电饭锅的风格的风格的刚的个","price":60,"image":"/images/good1.jpg","href":"http://localhost:3000/good/00002.html"}]);
 }
 exports.getGood = function (type_id, cb) {
     cb(null,{"name":"一个",
